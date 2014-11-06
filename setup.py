536643342429970f4c5074703c55e614d1fe7451
Haak Saxberg <haak.erling@gmail.com>
2014-11-05 22:23:20 -0800

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages
 
 setup(
     name="solid",
-    version='0.1.2',
+    version='0.1.3',
     description="Pythonic state machines",
     long_description=open("README.txt").read(),
     author="Haak Saxberg",
